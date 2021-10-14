894e5cd58898e23cbac15bf9682d4c41d27812bd
nicoContigliani <nico.contigliani@gmail.com>
2021-10-14 19:32:59 -0300

    <button onClick={() => { dispatch(obtenerPokemonesAccion()) }}>

--- src/componentes/Pokemones.jsx
+++ src/componentes/Pokemones.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { obtenerPokemonesAccion } from '../redux/PokeDucks';
+
 
 const Pokemones = () => {
+    const dispatch = useDispatch()
     return (
         <div>
             Lista de pokemons
+            <button onClick={() => { dispatch(obtenerPokemonesAccion()) }}>
+                Get Pokemons
+            </button>
         </div>
     )
 }
