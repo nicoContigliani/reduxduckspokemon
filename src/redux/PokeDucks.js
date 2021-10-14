e22940f50f2ace44d8f53dd9fcbd843c2755d05d
nicoContigliani <nico.contigliani@gmail.com>
2021-10-14 20:13:53 -0300

    offset

--- src/redux/PokeDucks.js
+++ src/redux/PokeDucks.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 //constantes  estados
 const dataInicial = {
-    array: []
+    array: [],
+    offset: 0,
+    limit: 30
+
 }
 
 //types
@@ -23,8 +26,12 @@ export default function pokeReducer(state = dataInicial, action) {
 
 //acciones
 export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
+    // console.log('getState', getState().pokemones.offset)
+    const offset = getState().pokemones.offset
+    const limit = getState().pokemones.limit
+
     try {
-        const res = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
         dispatch({
             type: OBTENER_POKEMONES_EXITO,
             payload: res.data.results
