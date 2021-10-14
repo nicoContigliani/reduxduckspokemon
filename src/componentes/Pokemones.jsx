ace67970282b1ff9f32105dd9280eb90ae7cdc74
nicoContigliani <nico.contigliani@gmail.com>
2021-10-14 19:44:44 -0300

    <ul>

--- src/componentes/Pokemones.jsx
+++ src/componentes/Pokemones.jsx
@@ -5,12 +5,28 @@ import { obtenerPokemonesAccion } from '../redux/PokeDucks';
 
 const Pokemones = () => {
     const dispatch = useDispatch()
+
+    const pokemones = useSelector(store => store.pokemones.array
+    )
+
+
     return (
         <div>
             Lista de pokemons
             <button onClick={() => { dispatch(obtenerPokemonesAccion()) }}>
                 Get Pokemons
             </button>
+
+
+            <ul>
+                {
+                    pokemones.map(item => (
+                        <li key={item.name}>{item.name}</li>
+                    ))
+                }
+            </ul>
+
+
         </div>
     )
 }
