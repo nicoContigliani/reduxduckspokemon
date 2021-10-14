442f68364c5b5497c8c16947423ccad8c8a82026
nicoContigliani <nico.contigliani@gmail.com>
2021-10-14 19:17:09 -0300

    preProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import react from "react";
 import Pokemones from "./componentes/Pokemones";
+import {Provider} from 'react-redux'
 
 
 function App() {
   return (
     <div >
+      
       <Pokemones />
     </div>
   );
